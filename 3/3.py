ba5a4bf2de2cffe2be1e577ef1310da1bb15637a
imane0897 <imane00897@gmail.com>
2016-12-19 21:26:50 +0800

    parameters renamed

--- 3/3.py
+++ 3/3.py
@@ -1,47 +1,40 @@
 import sys
 
-dic = {}
-new_dic = {}
-
-def fname():
+def remove(dic):
+    new_dic = {}
     dic_list = list(dic.keys())
-    for l, d in enumerate(dic_list):
-        print('current d is ', d)
+    for i, form_left in enumerate(dic_list):
 
-        for p in dic_list[:l]:
-            for v, s in enumerate(dic[str(d)]):
-                if s[0] == str(p):
-                    print (dic[str(p)][0])
-                    dic[str(d)][v] = dic[str(p)][0]
-                    for t in dic[str(p)][1:]:
-                        dic[str(d)].append(t)
+        for j in dic_list[:i]:
+            for index, form_right in enumerate(dic[str(form_left)]):
+                if form_right[0] == str(j):
+                    dic[str(form_left)][index] = dic[str(j)][0]
+                    for t in dic[str(j)][1:]:
+                        dic[str(form_left)].append(t)
                 else:
                     pass
 
         flag = 0
-        for i, s in enumerate(dic[str(d)]):
-            if s[0] == str(d)[0]:
-                temp = s[0]+'\''
+        for i, form_right in enumerate(dic[str(form_left)]):
+            if form_right[0] == str(form_left)[0]:
+                temp = form_right[0]+'\''
                 new_dic[temp] = []
                 flag = 1
                 break
-            else:
-                pass
-
 
         if flag == 1:
             i = 0
-            n = len(dic[str(d)])
+            n = len(dic[str(form_left)])
             n = int(n)
             i = int(i)
-            k=0
+            k = 0
             while i < n :
-                if dic[str(d)][k][0] == str(d)[0]:
-                    dic[str(d)].pop(i)
-                    new_dic[temp].append(s[1:]+temp)
+                if dic[str(form_left)][k][0] == str(form_left)[0]:
+                    dic[str(form_left)].pop(i)
+                    new_dic[temp].append(form_right[1:]+temp)
                     k=k-1
                 else:
-                    dic[str(d)][k]=dic[str(d)][k]+temp
+                    dic[str(form_left)][k]=dic[str(form_left)][k]+temp
                     flag2 = 0;
                     for b in new_dic[temp]:
                         if b == '#':
@@ -52,16 +45,18 @@ def fname():
                         pass
                     k=k+1
                 i = i+1
+    dic_merge(dic, new_dic)
 
 
 
-def merge():
+def dic_merge(dic, new_dic):
     for d in new_dic:
         dic[str(d)] = []
         for s in new_dic[str(d)]:
             dic[str(d)].append(s)
 
-def my_print():
+
+def print_forms(dic):
     for d in dic:
         temp = str(d)+'::='
         for s in dic[str(d)][:-1]:
@@ -69,15 +64,14 @@ def my_print():
         temp = temp + dic[str(d)][-1]
         print(temp)
 
+
 if __name__=='__main__':
+    dic = {}
     with open('/Users/AnYameng/Desktop/c/homework_of_compilers/3/3.txt', 'r') as f:
         for line in f.readlines():
             if line[0] in dic:
-
                 dic[line[0]].append(line[4:-1])
             else:
                 dic[line[0]] = [line[4:-1]]
-
-    fname()
-    merge()
-    my_print()
+    remove(dic)
+    print_forms(dic)
